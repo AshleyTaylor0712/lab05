eece688fe10d386c6e8962dde2aba8e3b43e6e25
Ashley Taylor <blueberrypea@gmail.com>
2023-05-15 13:35:55 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -98,7 +98,7 @@ class Header extends Component {
             />
           </Nav.Item>
         </Nav>
-        <div className="row aligner" style={{height: '100%', backgroundImage: 'url(img/home.png)'}}>
+        <div className="row aligner" style={{height: '100%', backgroundImage: 'url(pulbic/img/home.png)'}}>
           <div className="col-md-12">
             <div>
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
